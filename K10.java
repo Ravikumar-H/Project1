02fead344343d3f94ad4943ca943d13bea325d91
Ravikumar-H <ravikumar.23cs118@sode-edu.in>
2024-09-26 10:21:12 +0530

    Add files via upload

---
+++ K10.java
@@ -0,0 +1,33 @@
+public class K10{
+public static void main(String[] args){
+int num1=3;
+String info1="";
+String info2="";
+String info3="";
+info1=String.valueOf(num1);
+for(int i=1;i<11;i++){
+info2=String.valueOf(i);
+info3=String.valueOf(num1*i);
+System.out.println(info1+"*"+info2+"="+info3);
+}
+System.out.println();
+
+num1=4;
+for(int i=1;i<11;i++){
+info2=String.valueOf(i);
+info3=String.valueOf(num1*i);
+System.out.println(info1+"*"+info2+"="+info3);
+}
+System.out.println();
+}
+
+num1=5;
+for(int i=1;i<11;i++){
+info2=String.valueOf(i);
+info3=String.valueOf(num1*i);
+System.out.println(info1+"*"+info2+"="+info3);
+}
+System.out.println();
+}
+}
+}
