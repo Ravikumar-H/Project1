02b6eb2fccf3f4615c694d8288ae63c9d77b4ab9
Ravikumar-H <ravikumar.23cs118@sode-edu.in>
2024-11-27 18:18:00 +0530

    Update NestedClass.java

--- NestedClass.java
+++ NestedClass.java
@@ -10,12 +10,7 @@ class Outer{
 
 public class NestedClass {
 	public static void main(String[] args) {
-		String title0="Lab 8 program - Nested Class";
-		String title1="Develop a Java program to create an outer class with a function display.";
-		String title2="Create another class inside the outer class named inner with a function called display";
-		String title3="Call the two functions in the main class";
-		System.out.println(title0+"\n"+title1+"\n"+title2+"\n"+title3+"\n");
 		Outer o1=new Outer();
 		o1.display();
-		Outer.Inner i1=o1.new Inner();    // object is of type Outer.Inner
+		Outer.Inner i1=o1.new Inner();   
 		i1.display();}}
