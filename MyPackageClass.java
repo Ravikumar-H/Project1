679e787d7d35589911474281e9c7caae5c5cd4a2
Ravikumar-H <ravikumar.23cs118@sode-edu.in>
2024-11-28 10:09:18 +0530

    Add files via upload

---
+++ MyPackageClass.java
@@ -0,0 +1,8 @@
+package mypack;
+public class MyPackageClass {
+	public void displayMessage() {
+		System.out.println("This message is from the MyPackageClass inside the mypack package");
+
+	}
+}
+
